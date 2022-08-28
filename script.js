4d8a3c6f41b14bfed9e5c9dba8b6c7212ba7fd9e
Leticia Rodrigues <lerodriguessales@gmail.com>
2022-08-28 16:12:57 -0300

    modified: script.js

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ let seuVotoPara = document.querySelector('.divisao1-1 span');
 let cargo = document.querySelector('.divisao1-2 span');
 let descricao = document.querySelector('.divisao1-4');
 let aviso = document.querySelector('.divisao2');
-let lateral = document.querySelector('divisao1Right');
-let numeros = document.querySelector('divisao1-3');
+let lateral = document.querySelector('.divisao1Right');
+let numeros = document.querySelector('.divisao1-3');
 
 let etapaAtual = 0;
 let numero = '';
 
 function comecarEtapa() {
-    let etapas = etapas[etapaAtual];
+    let etapa = etapas[etapaAtual];
 
     let numeroHtml = '';
 
-    for (let i=0;i<etapas.numeros; i++) {
+    for (let i=0;i<etapa.numeros; i++) {
         numeroHtml += '<div class="numero"></div>';
     }
 
     seuVotoPara.style.display = 'none';
-    cargo.innerHTML = etapas.titulo;
+    cargo.innerHTML = etapa.titulo;
     descricao.innerHTML = '';
     aviso.style.display = 'none';
     lateral.innerHTML = '';
